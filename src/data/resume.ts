e67537c40098253afb0d870a09968108cd5ad76f
Rahul Kamble <rahulkamble@customfurnish.com>
2026-02-13 13:09:20 +0530

    rahul

--- src/data/resume.ts
+++ src/data/resume.ts
@@ -1,6 +1,6 @@
 export const resumeData = {
   personalInfo: {
-    name: "Rahul Kam",
+    name: "Rahul Kamble",
     role: "Salesforce Developer & Software Engineer",
     contact: {
       phone: "+91-8263069830",
